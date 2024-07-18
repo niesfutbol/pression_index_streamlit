35bcce0d0778af0a6c8cf3750f10187bd441b76f
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-07-17 21:38:04 -0700

    🎨 Format with Black

--- pression_index_st/scatter_plot.py
+++ pression_index_st/scatter_plot.py
@@ -1,11 +1,13 @@
 import altair as alt
 import pandas as pd
 
+
 def create_scatter_plot(data, x_axis, y_axis):
-    scatter_plot = alt.Chart(data).mark_circle(size=60).encode(
-        x=x_axis,
-        y=y_axis,
-        tooltip=[x_axis, y_axis]
-    ).interactive()
+    scatter_plot = (
+        alt.Chart(data)
+        .mark_circle(size=60)
+        .encode(x=x_axis, y=y_axis, tooltip=[x_axis, y_axis])
+        .interactive()
+    )
 
     return scatter_plot
